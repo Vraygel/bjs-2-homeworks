fcd615236b7aea2b1fa1808d329d83d18fe0b3f6
Владимир Александрович Райгель <vraygel@mail.ru>
2022-06-14 19:40:36 +0300

    1-я и 2-я задачи

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  
+  if(arr1.length == arr2.length){
+    result = arr1.every(totalTemp)
+ } else {result = false}
+ 
+ function totalTemp(item, index){
+  return item === arr2[index];
+}
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
+  let filterTemp 
 
-  // Ваш код
-
+  filterTemp = arr.filter((elem) => filterTemp = (elem >  0) && (elem % 3 == 0));
+  
+  resultArr = filterTemp.map((item) => item * 10);
+ 
   return resultArr; // array
 }
