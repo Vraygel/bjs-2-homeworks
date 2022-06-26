7b8b560df1f5634394ba9ef72f9ff625c4cc4449
Владимир Александрович Райгель <vraygel@mail.ru>
2022-06-26 18:29:09 +0300

    переработка 2 и 3 задач

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,7 +3,7 @@ function getArrayParams(arr) {
   let min = Infinity, max = -Infinity, sum = 0, avg = 0;
 
 for (let i=0; i < arr.length; i++){
-  
+
   sum = sum + arr[i]
 
   if(arr[i] < min){
@@ -21,9 +21,6 @@ avg = +avg.toFixed(2)
   return { min: min, max: max, avg: avg };
 }
 
-
-
-
 // Задание 2
 function worker(arr) {
   let sum = 0;
@@ -36,29 +33,17 @@ function worker(arr) {
 
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
-  
-  
-    for (i=0; i < arrOfArr.length; i++){
-       
-      if(worker(arrOfArr[i]) > max){
-        max = worker(arrOfArr[i])
+  for(let item of arrOfArr){
+      if(func(item) > max){
+        max = func(item)
       }
-      } 
-    
-      
-  return max;
+  }
+   return max
 }
-
 
 // Задание 3
 function worker2(arr) {
-
-  getArrayParams(arr)
-
-  }
-  
  
+  return (getArrayParams(arr)).max - (getArrayParams(arr)).min
 
-    
- 
- 
+}
