17a7213ad826778f865394e481a42661252bf24c
Владимир Александрович Райгель <vraygel@mail.ru>
2022-07-09 14:15:52 +0300

    8 задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,59 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let result = cache.find(item => item.hash === hash);
+    if (result) {
+      console.log("Из кэша: " + result.value);
+      return "Из кэша: " + result.value;
+    } else {
+      let value = func.call(this, ...args);
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      cache.push({ hash, value });
+      console.log("Вычисляем: " + value);
+      return "Вычисляем: " + value;
+    }
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let nextCall = true;
+  let interval;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  function wrapper(...args) {
+    if (nextCall) {
+      func.apply(this, ...args);
+      nextCall = false;
+    }
+    clearTimeout(interval);
+    interval = setTimeout(() => {
+      func.apply(this, ...args);
+      nextCall = true;
+    }, ms)
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let nextCall = true;
+  let interval;
+  wrapper.count = 0;
+
+  function wrapper(...args) {
+    wrapper.count++;
+    if (nextCall) {
+      func.apply(this, ...args);
+      nextCall = false;
+    }
+    clearTimeout(interval);
+    interval = setTimeout(() => {
+      func.apply(this, ...args);
+      nextCall = true;
+    }, ms)
+  }
+  return wrapper;
 }
